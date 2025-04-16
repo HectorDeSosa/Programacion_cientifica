802bccd9a3caa984cb49f1557bf410c8e2b68ccc
HectorDeSosa <hectordesosa.01@gmail.com>
2025-04-16 16:44:32 -0300

    punto2y8

---
+++ punto2.py
@@ -0,0 +1,11 @@
+# De Sosa Hector
+# 2025
+# Programacion Cientifica
+# CONTENIDO del Punto:
+#   Definir dos variables de tipo string y sumarlas. ¿Qué resultados obtuvo?
+#Respuesta:
+
+primera = "string1 "
+segunda = "string2"
+
+print(primera+segunda)
---
+++ punto8.py
@@ -0,0 +1,21 @@
+# De Sosa Hector
+# 2025
+# Programacion Cientifica
+# CONTENIDO del Punto:
+#   Pide dos números y muestra la suma, resta y multiplicación de ambos.
+#Respuesta:
+
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = float(input('Ingrese el segundo numero: '))
+
+suma = numero1 + numero2
+
+resta = numero1 - numero2
+
+multiplicacion = numero1 * numero2
+
+print("La suma es: ", suma)
+
+print(f'La resta es: {resta}')
+
+print('La multiplicacion es: {0}'.format(multiplicacion))
