dfc608c114007978f6890d0a018c2551bf8a3467
HectorDeSosa <hectordesosa.01@gmail.com>
2025-06-15 14:46:04 -0300

    listar productos

--- productos.py
+++ productos.py
@@ -1,7 +1,12 @@
-# cargar productos
+# listar productos
 def listar_producto(inventario):
     if not inventario:
         print("No hay productos en el inventario.")
     else:
         for nombre, datos in inventario.items():
             print(f"Producto: {nombre}, Precio: {datos['precio']}, Cantidad: {datos['cantidad']}")
+
+#if __name__ == "__main__":
+    #este modulo de por si no tiene sentido ejecutarlo
+    #por lo tanto estas lineas no hacen falta
+    #listar_producto(inventario)
