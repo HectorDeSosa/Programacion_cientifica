07b2d80497cbef2067a5acb7f1e25e3fd1e80283
HectorDeSosa <hectordesosa.01@gmial.com>
2025-09-05 10:31:21 -0300

    BlogSite

---
+++ blogsite/notas/models.py
@@ -0,0 +1,10 @@
+# Create your models here.
+from django.db import models
+
+class Nota(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
