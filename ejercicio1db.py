e29453d4402b82239e24ff5f771797943b8f10f5
HectorDeSosa <hectordesosa.01@gmial.com>
2025-09-17 21:37:01 -0300

    ejercicio1db

---
+++ ejercicio1db.py
@@ -0,0 +1,47 @@
+import sqlite3
+from sqlite3 import Error
+
+
+#definicion de tabla
+create_tabla_cliente = """
+CREATE TABLE CLIENTE
+(
+  NumCliente INT NOT NULL,
+  nombre CHAR(20) NOT NULL,
+  direccion VARCHAR(50) NOT NULL,
+  PRIMARY KEY (NumCliente)
+);
+"""
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("\nConexion SQLite DB exitosa")
+    except Error as e:
+        print(f"Ocurrio un error'{e}'")
+    return connection
+def execute_query(connection, consulta):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(consulta)
+        connection.commit()
+        print("Consulta ejecutada exitosamente")
+    except Error as e:
+        print(f"Ocurrio un error'{e}'")
+#crear conexion
+connection = create_connection("ejercicio1db.sqlite")
+#ejecutar una consulta o un comando
+execute_query(connection, create_tabla_cliente)
+
+create_client = """
+INSERT INTO
+  CLIENTE (NumCliente, nombre, direccion)
+VALUES
+  (1, 'juan', 'USA'),
+  (2, 'luis', 'AR'),
+  (3, 'pedro', 'BR'),
+  (4, 'tito', 'MX')
+"""
+
+execute_query(connection, create_client)
