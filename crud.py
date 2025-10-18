fe489ba484ab1b9dd53d8477740d207b28a099da
HectorDeSosa <hectordesosa.01@gmial.com>
2025-10-18 17:28:38 -0300

    crud1

---
+++ crud.py
@@ -0,0 +1,5 @@
+#se puede clonar el repositorio o ir a mirar para tener de referencia
+#https://github.com/yjg30737/pyqt-database-example/tree/main
+
+#2 estilo de vista
+#https://realpython.com/python-contact-book/
